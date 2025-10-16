4601ab0c8bd010ce41aa2711867afe500251693a
Bdhf-Hdhfj <140914032+Bdhf-Hdhfj@users.noreply.github.com>
2025-10-16 15:28:46 +0000

    Update

---
+++ migrations/insert_data.sql
@@ -0,0 +1,18 @@
+-- insert data generated from CSV
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生人數', '人數', 40666, 40666, 49026, 49026);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 35/34/33', '人數', 283, 283, 296, 296);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 35/34/33', '百分比', 0.7, 0.7, 0.6, 0.6);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 32/31/30', '人數', 643, 926, 671, 967);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 32/31/30', '百分比', 1.6, 2.3, 1.4, 2);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 29/28/27', '人數', 1253, 2179, 1306, 2273);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 29/28/27', '百分比', 3.1, 5.4, 2.7, 4.6);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 26/25/24', '人數', 2138, 4317, 2256, 4529);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 26/25/24', '百分比', 5.3, 10.6, 4.6, 9.2);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 23/22/21', '人數', 3648, 7965, 3801, 8330);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 23/22/21', '百分比', 9, 19.6, 7.8, 17);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 20/19/18', '人數', 4887, 12852, 5109, 13439);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 20/19/18', '百分比', 12, 31.6, 10.4, 27.4);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 17/16/15', '人數', 4011, 16863, 4187, 17626);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 17/16/15', '百分比', 9.9, 41.5, 8.5, 36);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 14/13/12', '人數', 1164, 18027, 1224, 18850);
+INSERT INTO hkdse_stats (range, category, day_count, day_cum_count, all_count, all_cum_count) VALUES ('考生取得 (332A)22 或以上最佳五科總積點 - 14/13/12', '百分比', 2.9, 44.3, 2.5, 38.4);
